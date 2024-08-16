9dc1a157d952a7b15fcea446cbf463dfa84373e6
lee93 <dohuny2@naver.com>
2024-08-17 04:09:13 +0900

    게시물 비밀번호 암호화

--- src/main/java/com/lee93/apiboard/service/PostService.java
+++ src/main/java/com/lee93/apiboard/service/PostService.java
@@ -4,24 +4,30 @@ import com.lee93.apiboard.dao.PostDAO;
 import com.lee93.apiboard.vo.PostRegisterVO;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 public class PostService {
     Logger logger = LoggerFactory.getLogger(this.getClass());
     private final PostDAO postDAO;
+    private final PasswordEncoder passwordEncoder;
 
-    public PostService(PostDAO postDAO) {
+    public PostService(PostDAO postDAO, PasswordEncoder passwordEncoder) {
         this.postDAO = postDAO;
+        this.passwordEncoder = passwordEncoder;
     }
+
 
     public void postRegister(PostRegisterVO postRegisterVO) {
         logger.info(" ## postRegister() 실행 ");
-        // 비밀번호 확인
-        
-        // 유효성 검사
-        
-        // 게시물 등록
+
+        String rawPassword = postRegisterVO.getPostPw();
+        String encodedPassword = passwordEncoder.encode(rawPassword);
+        postRegisterVO.setPostPw(encodedPassword);
+        logger.info("Encoded Password Check: {} ", postRegisterVO.getPostPw());
+        // TODO 유효성 검사하기
         postDAO.postRegister(postRegisterVO);
     }
+
 }
